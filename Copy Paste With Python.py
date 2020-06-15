b6e52f22f22506a47d7851c1cc61f1d07a1bbbc8
Sarthak Bhutani <isarthakbhutani@gmail.com>
2020-06-16 01:41:46 +0530

    frst

---
+++ Copy Paste With Python.py
@@ -0,0 +1,6 @@
+#this can be done by using Pyprclip module, means copy paste text in python drom pyhton clipboard
+import pyperclip
+
+#now copy anything frm mouse/keyboard from anywhere in your pc and then run this prg
+print(pyperclip.paste())
+
